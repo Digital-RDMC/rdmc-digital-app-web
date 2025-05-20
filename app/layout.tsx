0e344db1faa931c0bb2886b7eec7d1521f08adb5
Digital-RDMC <digital.rdmc@ratpdev.com>
2025-05-20 21:03:17 +0300

    deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,18 +3,14 @@
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "@/components/ui/sonner"
 // import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Inter } from "next/font/google";
 import "./globals.css";
+import "./fonts.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const inter = Inter({
+//   subsets: ["latin"],
+//   variable: "--font-inter",
+// });
 
 // export const metadata: Metadata = {
 //   title: "Create Next App",
@@ -26,12 +22,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-      <SessionProvider>
-
+  return (      <SessionProvider>
  <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className="antialiased"
       >
         {children}
 
